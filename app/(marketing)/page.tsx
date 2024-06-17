ea4a982559474bb7564e896316d68be66b148a1a
Rohit7825 <rohitshekhar7825@gmail.com>
2024-06-17 19:59:58 +0530

    skeleton for the landing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button size="lg">Click me</Button>
+      <Button size="lg">This is a marketin g page</Button>
     </div>
   );
 }
